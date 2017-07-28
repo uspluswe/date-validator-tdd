254b80a5cd495950ec48130fd26a34d1640b7eaf
uspluswe <uspluswe@gmail.com>
2017-07-28 15:10:54 -0600

    adding tdd

--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
-function add(a, b) {
-  return a + b;
+function isValidDate(date){
+  return hasValidFormat(date);
+  return hasMatchingSeparators(date);
+}
+
+function hasValidFormat(date){
+  var dateReg = /^([0][1-9]|[1][0-2])[./-]([0][1-9]|[1]\d{1}|[2]\d{1}|[3][0-1])[./-](\d{2}$|[1][8-9]\d{2}|[2][0][0-1][0-7])$/;
+  return dateReg.test(date);
+}
+
+function hasMatchingSeparators(date){
+  return date.charAt(2) === date.charAt(5);
+}
+
+function hasValidMonthLengths(date){
+  var monthStr = date.substr(0,2);
+  var monthNum = Number(monthStr);
+  var dayStr = date.substr(3,4);
+  var dayNum = Number(dayStr);
+  if (monthNum === 1 && dayNum < 32) {
+    return true;
+  } else if (monthNum === 2){
+      if (leapYear(date) === true && dayNum < 30) {
+        return true;
+      } else if (leapYear(date) === false && dayNum < 29) {
+        return true
+      }
+  } else if (monthNum === 3  && dayNum < 32){
+    return true;
+  } else if (monthNum === 4  && dayNum < 31){
+    return true;
+  } else if (monthNum === 5 && dayNum < 32){
+    return true;
+  } else if (monthNum === 6 && dayNum < 31){
+    return true;
+  } else if (monthNum === 7 && dayNum < 32){
+    return true;
+  } else if (monthNum === 8 && dayNum < 32){
+    return true;
+  } else if (monthNum === 9 && dayNum < 31){
+    return true;
+  } else if (monthNum === 10 && dayNum < 32){
+    return true;
+  } else if (monthNum === 11 && dayNum < 31){
+    return true;
+  } else if (monthNum === 12 && dayNum < 32){
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function leapYear(date)
+{
+  var yearStr = date.substr(6);
+  var year = Number(yearStr);
+  return ((year % 4 === 0) && (year % 100 != 0)) || (year % 400 === 0);
 }
--- tests/test.spec.js
+++ tests/test.spec.js
@@ -1,7 +1,46 @@
-describe('Test', () => {
-  describe('add', () => {
-    it('should add two numbers', () => {
-      expect(add(2, 3)).toBe(5);
-    });
-  });
+describe('isValidDate', function(){
+  describe('hasValidFormat', function(){
+    it('should only accept numbers as dates', function(){
+      expect(hasValidFormat('f/08/09')).toBe(false); // invalid - has a letter
+      expect(hasValidFormat('09----09')).toBe(false); // invalid - has invalid characters
+      expect(hasValidFormat('february')).toBe(false); // invalid - is a word
+      expect(hasValidFormat('09.08.09')).toBe(true); // valid - has only numbers
+      })
+    it('should be the correct length', function(){
+      expect(hasValidFormat('02/01/19934')).toBe(false); // invalid - too long
+      expect(hasValidFormat('02-01-034')).toBe(false); // invalid - too short
+      expect(hasValidFormat('12.07.1993')).toBe(true); // valid - 10 characters
+      expect(hasValidFormat('07-11-15')).toBe(true); // valid - 8 characters
+        })
+    it('should have valid month numbers (01-12)', function(){
+      expect(hasValidFormat('51/07/1993')).toBe(false); // invalid - invalid month number
+      expect(hasValidFormat('12-07-1993')).toBe(true); // valid - valid month number
+    })
+    it('should have valid days in the month (01-31)', function(){
+      expect(hasValidFormat('12-35-1963')).toBe(false); // invalid - invalid day
+      expect(hasValidFormat('04-03-02')).toBe(true); // valid - valid day
+    })
+    it('should only include the last 120 years', function(){
+      expect(hasValidFormat('07.04.1776')).toBe(false); // invalid - not within the last 120 years
+      expect(hasValidFormat('07-11-2015')).toBe(true); // valid - within the last 120 years
+    })
+    it('should have valid date separators', function(){
+      expect(hasValidFormat('02:19:2000')).toBe(false); // invalid - invalid separators
+      expect(hasValidFormat('09/14/1987')).toBe(true); // valid - valid separators
+    })
+  })
+
+  describe('hasMatchingSeparators', function(){
+    it('should only have matching separators', function(){
+      expect(hasMatchingSeparators('09.17-2017')).toBe(false); // invalid - mixed separators
+      expect(hasMatchingSeparators('11-24-2016')).toBe(true); // valid - matching separators
+    })
+  })
+
+  describe('hasValidMonthLengths', function(){
+    it('should have valid month lengths', function(){
+      expect(hasValidMonthLengths('05-36-1962')).toBe(false); // invalid - month too long
+      expect(hasValidMonthLengths('06.07.2008')).toBe(true); // valid - valid month length
+    })
+  })
 });
